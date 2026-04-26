068f1123542f8c9cb98144ef698cd793d15adfee
Rafael (Falves) Alves <rafalves106@icloud.com>
2026-04-25 22:34:56 -0300

    alterações

--- site/vite.config.js
+++ site/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         obrigado: resolve(__dirname, "obrigado.html"),
+        executivo: resolve(__dirname, "executivo.html"),
       },
     },
   },
